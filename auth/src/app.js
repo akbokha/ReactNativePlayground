6e50a1bb8d8aed2b8f08342ce5b7189e52c49973
Abdel K. Bokharouss <a.k.bokharouss@student.tue.nl>
2018-06-24 15:07:10 +0200

    firebase set-up

--- auth/src/app.js
+++ auth/src/app.js
@@ -3,9 +3,21 @@ import {
     View,
     Text
 } from 'react-native';
+import firebase from 'firebase';
 import {Header, Section, SectionHolder} from "./components/common";
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyDc1HimSpf_TUK7zv3MNsXcdhmRl0RqV-Y',
+            authDomain: 'auth-e4671.firebaseapp.com',
+            databaseURL: 'https://auth-e4671.firebaseio.com',
+            projectId: 'auth-e4671',
+            storageBucket: 'auth-e4671.appspot.com',
+            messagingSenderId: '851417185571'
+        });
+    }
+
     render() {
         return (
             <View style={{flex: 1}}>
