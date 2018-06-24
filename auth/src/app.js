c81bc35fac65807986466915797779912410172b
Abdel K. Bokharouss <a.k.bokharouss@student.tue.nl>
2018-06-24 15:55:44 +0200

    workaround for firebase/firebase-js-skd/#908 and structure loginform

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,34 +1,35 @@
 import React, {Component} from 'react';
 import {
-    View,
-    Text
+    View
 } from 'react-native';
-import firebase from 'firebase';
-import {Header, Section, SectionHolder} from "./components/common";
+import LoginForm from './components/loginform';
+import {Header} from "./components/common";
 
 class App extends Component {
-    componentWillMount() {
-        firebase.initializeApp({
+    initializeFirebase() {
+        const firebase = require("firebase");
+
+        // Initialize Firebase
+        var config = {
             apiKey: 'AIzaSyDc1HimSpf_TUK7zv3MNsXcdhmRl0RqV-Y',
             authDomain: 'auth-e4671.firebaseapp.com',
             databaseURL: 'https://auth-e4671.firebaseio.com',
             projectId: 'auth-e4671',
             storageBucket: 'auth-e4671.appspot.com',
             messagingSenderId: '851417185571'
-        });
+        };
+        firebase.initializeApp(config);
+    }
+
+    componentWillMount() {
+        this.initializeFirebase();
     }
 
     render() {
         return (
             <View style={{flex: 1}}>
-                <Header headerText="Simple Authorization"/>
-                <SectionHolder>
-                    <Section>
-                        <Text>
-                            Lorem Ipsum
-                        </Text>
-                    </Section>
-                </SectionHolder>
+                <Header headerText={"Simple Authorization"}/>
+                <LoginForm/>
             </View>
         );
     }
---
+++ auth/src/components/loginform.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {
+    View
+} from 'react-native';
+import {Section, SectionHolder, Button} from "./common";
+
+class LoginForm extends Component {
+    render() {
+        return (
+            <SectionHolder>
+                {/*<Section>*/}
+
+                {/*</Section>*/}
+                {/*<Section>*/}
+
+                {/*</Section>*/}
+                {/*<Section>*/}
+                {/*<Button>Log in</Button>*/}
+                {/*</Section>*/}
+            </SectionHolder>
+
+        );
+    }
+}
+
+export default LoginForm;
