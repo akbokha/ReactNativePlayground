48843227b9ae775d43c99401d1e984ae7b014e47
Abdel K. Bokharouss <a.k.bokharouss@student.tue.nl>
2018-07-15 22:11:05 +0200

    Added picker for the shifts

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Picker } from 'react-native'
 import { connect } from 'react-redux'
+import { employeeUpdate } from "../actions";
 import { Card, CardSection, Input, Button } from "./common";
 
 class EmployeeCreate extends Component {
@@ -11,17 +12,37 @@ class EmployeeCreate extends Component {
           <Input
             label="Name"
             placeholder="Mark Zuckerberg"
+            value={this.props.name}
+            onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
           />
         </CardSection>
+
         <CardSection>
           <Input
             label="Phone"
             placeholder="06 12345678"
+            value={this.props.phone}
+            onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
           />
         </CardSection>
-        <CardSection>
 
+        <CardSection style={{ flexDirection: 'column' }}>
+          <Text style={styles.shiftTextStyle}>Shift</Text>
+          <Picker
+            style={{ flex: 1 }}
+            selectedValue={this.props.shift}
+            onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
+          >
+            <Picker.Item label="Monday" value="mon"/>
+            <Picker.Item label="Tuesday" value="tue"/>
+            <Picker.Item label="Wednesday" value="wed"/>
+            <Picker.Item label="Thursday" value="thu"/>
+            <Picker.Item label="Friday" value="fri"/>
+            <Picker.Item label="Saturday" value="sat"/>
+            <Picker.Item label="Sunday" value="sun"/>
+          </Picker>
         </CardSection>
+
         <CardSection>
           <Button>
             Create
@@ -32,4 +53,16 @@ class EmployeeCreate extends Component {
   }
 }
 
-export default EmployeeCreate
+const styles = {
+  shiftTextStyle: {
+    fontSize: 18,
+    paddingLeft: 20
+  }
+}
+
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.emp
+  return { name, phone, shift }
+}
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate)
